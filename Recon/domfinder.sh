bdc2c823f7ba1c9c608c2f482c60f653990ee29a
jerinshajit97 <jerinshajit97@gmail.com>
2021-01-22 15:15:52 +0530

    modifed domfinder

--- Recon/domfinder.sh
+++ Recon/domfinder.sh
@@ -4,7 +4,12 @@ echo "[+] Starting domfinder"
 mkdir -p assets
 for d in $(cat $1)
 	do
-	   assetfinder $d | unfurl format %r.%t | sort -u > $d.txt
+	   echo "[-] searching assets of $d"
+	   
+	   assetfinder $d | unfurl format %r.%t | sort -u > assets/$d.txt
+
+	   echo "[-] $d completed"
+	   echo "[-] found $(cat assets/$d.txt | wc -l) for $d"
 	done
 
 echo "[+] mission accomplished, Peace Be With You"
